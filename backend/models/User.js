bd56ff9594922730b5a680c5c3d66e073fb6c1ab
Dhruv-232 <118102311+Dhruv-232@users.noreply.github.com>
2023-05-07 13:40:44 +1000

    email and password in user schema is done

--- backend/models/User.js
+++ backend/models/User.js
@@ -6,6 +6,16 @@ const UserSchema = new mongoose.Schema({
         required: true,
         unique: true,
     },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        min: 6,
+    },
     
 }, )
 
