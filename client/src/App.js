d720a0b0e2daa14e1ffeb330864979970aef43de
Dhruv-232 <118102311+Dhruv-232@users.noreply.github.com>
2023-05-08 19:41:59 +1000

    Added dummy link to switch between routes it works now

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
-import {BrowserRouter , Routes , Route}
-  from "react-router-dom"
+import {BrowserRouter , Routes , Route} from "react-router-dom"
 import Login from "./components/login"
 import Home from "./components/Home"
 
--- client/src/components/login.js
+++ client/src/components/login.js
@@ -1,9 +1,10 @@
-
+import { Link } from "react-router-dom"
 
 function Login() {
     return (
         <div>
             login page
+            <Link to= "/home">GO TO HOME</Link>
         </div>
     )
 }
