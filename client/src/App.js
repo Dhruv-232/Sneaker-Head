61fc0db76a1ac0a4cb583621b346f2924cb064e0
Dhruv-232 <118102311+Dhruv-232@users.noreply.github.com>
2023-05-08 19:37:32 +1000

    Made home.js route and tested it works

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,14 @@ import './App.css';
 import {BrowserRouter , Routes , Route}
   from "react-router-dom"
 import Login from "./components/login"
+import Home from "./components/Home"
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
       <Route path = '/login' element={ <Login /> } />
-      <Route path = '/home' element={ <Login /> } />
+      <Route path = '/home' element={ <Home /> } />
     </Routes>
     </BrowserRouter>
   );
