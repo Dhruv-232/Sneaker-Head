33ca9e7f1290b7e4868d34f536c6875045785424
Dhruv-232 <118102311+Dhruv-232@users.noreply.github.com>
2023-05-08 20:51:51 +1000

    Finally figured how to console username and password for further use in API

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -1,18 +1,26 @@
+import {useState} from "react"
 import { Link } from "react-router-dom"
 
 function Login() {
+
+    const [userName, setUserName] = useState('')
+    const [password, setPassword] = useState('')
+    const handleLogin = () => {
+        console.log(userName, password)
+    }
     return (
         <div>
             <h1> LOGIN PAGE </h1>
             USERNAME -
-            <input type = "text"/>
-            <br />
-            <br />
+            <input type = "text" value={userName} onChange={(e) => { 
+                setUserName(e.target.value)
+            }} /> <br /> <br />
             PASSWORD - 
-            <input type = "text"/>
-            <br />
-            <br />
-            <button> SUBMIT </button>
+            <input type = "text" value={password} onChange={(e) => { 
+                setPassword(e.target.value)
+            }}/> <br /> <br />
+
+            <button onClick={handleLogin}> SUBMIT </button>
             
         </div>
     )
