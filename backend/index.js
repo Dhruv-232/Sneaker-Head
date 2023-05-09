fec8a17b3e13053969bf3b9d77c1ee7d9fefef69
Dhruv-232 <118102311+Dhruv-232@users.noreply.github.com>
2023-05-09 12:02:08 +1000

    Tested simple get API works

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3001
 
-app.get('/', (req, res) => {
-    res.send('Hello World')
+app.get('/hello', (req, res) => {
+    res.send({ code: '200', message: 'hello world' })
 })
 
 app.listen(port, () => {
